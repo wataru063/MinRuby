e9a541e13a4d4d60bf9d4229514418721964025e
skgch0702 <skgch0702@gmail.com>
2023-01-17 16:30:25 +0900

    add 4th

---
+++ 4th/interp.rb
@@ -0,0 +1,23 @@
+require "minruby"
+
+def evaluate(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+"
+    evaluate(tree[1]) + evaluate(tree[2])
+  when "-"
+    evaluate(tree[1]) - evaluate(tree[2])
+  when "*"
+    evaluate(tree[1]) * evaluate(tree[2])
+  when "/"
+    evaluate(tree[1]) / evaluate(tree[2])
+  end
+end
+
+str = gets
+
+tree = minruby_parse(str)
+
+answer = evaluate(tree)
+p(answer)
